9fea74c19c0f79d6033c81e78584a1af50a1b7a0
DebugSteven <debugsteven@gmail.com>
2018-11-30 14:21:39 -0500

    code comments

--- src/lib.rs
+++ src/lib.rs
@@ -54,10 +54,13 @@ impl Universe {
         (row * self.width + column) as usize
     }
 
+    /// Get the dead and alive values of the entire universe.
     pub fn get_cells(&self) -> &[Cell] {
         &self.cells
     }
 
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
     pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
         for (row, col) in cells.iter().cloned() {
             let idx = self.get_index(row, col);
@@ -185,6 +188,9 @@ impl Universe {
         self.width
     }
 
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
     pub fn set_width(&mut self, width: u32) {
         self.width = width;
         self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
@@ -194,6 +200,9 @@ impl Universe {
         self.height
     }
 
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
     pub fn set_height(&mut self, height: u32) {
         self.height = height;
         self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
